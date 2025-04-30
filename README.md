51b7017a7c1b6435fbcaceacb7150b69078b485d
Kliver Giron <kliver.giron@dfinsolutions.com>
2025-04-30 18:36:48 -0500

    Save drivers as dependencies

--- README.md
+++ README.md
@@ -6,7 +6,9 @@ First install dependencies
 ```
 npm install
 ```
-Second you need to create `.env` file in root with
+Second you need to create a Trello account (please cretae and do not use an old one) and setup to be ready to work.
+
+Third you need to create `.env` file in root with information of recently created account
 ```
 TRELLO_USERNAME="example@email.com"
 TRELLO_PASSWORD="your_password"
--- wdio.conf.js
+++ wdio.conf.js
@@ -66,6 +66,9 @@ exports.config = {
                     ...(isHeadless ? ['-headless'] : []),
                     '-width=1920', '-height=1080'
                 ]
+            } : undefined,
+            'safari.options': BROWSER === 'safari' ? {
+                technologyPreview: false
             } : undefined
         }
     ],
@@ -117,7 +120,10 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    // services: [],
+    services: [
+        ...(BROWSER === 'chrome' ? ['chromedriver'] : []),
+        ...(BROWSER === 'firefox' ? ['geckodriver'] : [])
+    ],
     //
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
